cd4d58c8b4b8c284b37f33fca3127f941ac499ea
Yury Kirilyuk <yury@cyberiansoft.com>
2020-05-21 03:09:52 +0300

    changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -34,8 +34,21 @@ jQuery(function($){
     $('.et_pb_team_member').on('click', function(){
         var el = $(this);
         showPopup(el);
-
     });
+
+    $('#brad').on('click', function(){
+        var el = $('#bradC');
+        showPopup(el);
+        return false;
+    });
+
+    $('.single-bio .et_pb_button').on('click', function(){
+        var el = $('.single-bio .et_pb_team_member');
+        showPopup(el);
+        return false;
+    });
+
+
     $('.et_pb_team_member .close').on('click', function(){
         var el = $(this);
         setTimeout(hidePopup, 300, el);
@@ -43,12 +56,12 @@ jQuery(function($){
 
 	function showPopup(el){
         $('body').addClass('popup-open');
-        el.addClass('popup').parents('.et_pb_section').css('z-index','10');
+        el.addClass('popup').parents('.et_pb_row, .et_pb_section').css('z-index','10');
     }
 
 	function hidePopup(el){
         $('body').removeClass('popup-open');
-        el.closest('.et_pb_team_member ').removeClass('popup').parents('.et_pb_section').css('z-index','0');
+        el.closest('.et_pb_team_member ').removeClass('popup').parents('.et_pb_row, .et_pb_section').css('z-index','0');
     }
 
 
@@ -70,6 +83,48 @@ jQuery(function($){
             e.preventDefault();
             return false;
         });
+
+        $('.section-upcoming-shows .et_pb_blurb').each(function(){
+            var $item = $(this);
+
+            $item.find('.et_pb_main_blurb_image').append($item.find('.et_pb_blurb_description'));
+        });
+
+
+        $('a[href^="popup"]').on('click', function(e){
+            var $el = $(this),
+                popup = $el.attr('href');
+
+            showPopup2(popup);
+
+            e.preventDefault();
+            return false;
+        });
+
+        $('.et_pb_main_blurb_image').on('click', function(e){
+            var $el = $(this);
+
+            hidePopup2($el);
+        });
+
+
+        function showPopup2(popup){
+            $('#' + popup).addClass('show').parents('.et_pb_row, .et_pb_section').css('z-index','10');
+        }
+
+        function hidePopup2($el){
+            $el.closest('.et_pb_blurb').removeClass('show').parents('.et_pb_row, .et_pb_section').css('z-index','0');
+        }
+
+
+
+
+
+
+
+
+        //$(".section-upcoming-shows .et_pb_main_blurb_image").appendTo(".section-upcoming-shows .et_pb_blurb_description");
+
 
 
         /*
